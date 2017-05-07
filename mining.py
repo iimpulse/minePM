d3edbe1a2bdace7663a6f52a83527175cacd5649
Mike Gargano <mgdlxd@gmail.com>
2017-05-07 18:49:29 -0400

    Reworking list creation. Less i/o

--- mining.py
+++ mining.py
@@ -1,9 +1,8 @@
 import re, csv,string
 from pymongo import MongoClient
 from multiprocessing import Pool
-
+from itertools import repeat
 class geneMatrix:
-
 	def __init__(self):
 		self.matrix = [[ x for x in range(11)] for x in range(39799)]
 		self.matrix[0][0] = "GeneSymbol"
@@ -45,6 +44,29 @@ class geneMatrix:
 					self.matrix[count][num] = 0
 					self.matrix[count][article] = "NA"
 
+
+class geneInit:
+	def __init__(self):
+		self.list = self.returnGeneList()
+		self.dict  = self.create_dict()
+        def returnGeneList(self):
+	    with open('./data/genes-new.txt','rb') as csvfile:
+		genereader = csv.reader(csvfile,delimiter='\n')
+		genesym = []
+		for row in genereader:
+		   row = row[0].split()
+		   row = row[0]
+		   genesym.append(row) 
+		genesym.pop(0)
+		return genesym
+	def create_dict(self):
+		gene = self.list
+  		gene_dict = {}
+  		for  i in range(0,len(gene)):
+    			gene_dict[str(gene[i])] = [0] 
+  		return gene_dict
+
+				
  
 def connectDB():
     client = MongoClient('mongodb://localhost:27017/')
@@ -52,8 +74,9 @@ def connectDB():
     return db
 
 
-def extractGeneData(cancer):
-    gene_dict = reset_dict() 
+def extractGeneData((cancer,geneDict)):
+    gene_dict = geneDict
+    #gene_dict = reset_dict() 
     #open db connection,get the cancer collection
     #loop through collection
     #Do original analysis
@@ -62,66 +85,50 @@ def extractGeneData(cancer):
     db = connectDB()
     count = 0 
     if(cancer == "bladder"):
-        collection = db.bladdercancer.find()
+	collection = db.bladdercancer.find()
     elif(cancer == "lung"):
-        collection = db.lungcancer.find()
+	collection = db.lungcancer.find()
     elif(cancer == "prostate"):
-        collection = db.prostatecancer.find()
+	collection = db.prostatecancer.find()
     elif(cancer == "colon"):
-        collection = db.coloncancer.find()
+	collection = db.coloncancer.find()
     elif(cancer == "pancreatic"):
-        collection = db.pancreaticcancer.find()
+	collection = db.pancreaticcancer.find()
     for x in collection: 
-    	abst =  x['ab']
-        abst = abst.split(" ")
-        abst = map(lambda s: s.encode('ascii','ignore').upper(),abst)
-        abst = map(lambda s: s.strip(string.punctuation),abst)
-        abst = set(abst)
-        abst = map(lambda s: s,abst)
-        pmid = x["pmid"].encode('ascii','ignore')
-        for i in range(0,len(abst)):
-            word = abst[i].strip()
-            #print word
-            try:
-                valueMatch = gene_dict[word]
-                if (valueMatch != 0):
-                    valueMatch[0] = valueMatch[0] + 1
-                    valueMatch.append(pmid)
-                    gene_dict[word] = valueMatch
-                else:
-                    newlist = []
-                    newlist.append(1)
-                    newlist.append(pmid)
-                    gene_dict[word] = newlist
-            except Exception,e:
-                continue
+	abst =  x['ab']
+	abst = abst.split(" ")
+	abst = map(lambda s: s.encode('ascii','ignore').upper(),abst)
+	abst = map(lambda s: s.strip(string.punctuation),abst)
+	abst = set(abst)
+	abst = map(lambda s: s,abst)
+	pmid = x["pmid"].encode('ascii','ignore')
+	for i in range(0,len(abst)):
+	    word = abst[i].strip()
+	    #print word
+	    try:
+		valueMatch = gene_dict[word]
+		if (valueMatch != 0):
+		    valueMatch[0] = valueMatch[0] + 1
+		    valueMatch.append(pmid)
+		    gene_dict[word] = valueMatch
+		else:
+		    newlist = []
+		    newlist.append(1)
+		    newlist.append(pmid)
+		    gene_dict[word] = newlist
+	    except Exception,e:
+		continue
     return gene_dict
-    
-def returnGeneList():
-    with open('./data/genes-new.txt','rb') as csvfile:
-        genereader = csv.reader(csvfile,delimiter='\n')
-        genesym = []
-        for row in genereader:
-           row = row[0].split()
-           row = row[0]
-           genesym.append(row) 
-        genesym.pop(0)
-        return genesym
 
 def writecsv(data):
-    csvfile = "./matrix.csv"
+    csvfile = "./matrix-test.csv"
     with open(csvfile, "w") as output:
       writer = csv.writer(output, lineterminator='\n')
       writer.writerows(data)
-def reset_dict():
-  gene = returnGeneList()
-  gene_dict = {}
-  for  i in range(0,len(gene)):
-    gene_dict[str(gene[i])] = [0] 
-  return gene_dict
-
+    print "Finished Writing."
 def row_find_init():
-    gene = returnGeneList();
+    gene = geneInit()
+    gene = gene.list
     row_find = {}
     for i in range(0,len(gene)):
         row_find[str(gene[i])] = 0
@@ -130,7 +137,7 @@ def row_find_init():
 def readcsv():
     db = connectDB()
     flag = 0
-    with open("./matrix.csv",'r') as dat:
+    with open("./matrix-test.csv",'r') as dat:
         reader = csv.reader(dat)
         for row in reader:
             flag = flag + 1
@@ -150,10 +157,12 @@ def main():
     # for cancer create a threader and call extract gene data
     # when one returns, inset into the matrix.
     miner = geneMatrix()
+    geneDict = geneInit()
+    geneDict = geneDict.dict
     miner.row_find = row_find_init()	#gene dictionairy for row position
-    pool = Pool(processes = 5)
     cancers = ["bladder","lung","prostate","colon","pancreatic"]
-    result = pool.map(extractGeneData,cancers)
+    pool = Pool(processes = len(cancers))
+    result = pool.map(extractGeneData,zip(cancers,repeat(geneDict)))
     pool.close()
     print "Setting Matrix, Writing Matrix"
     miner.setMatrix(result[0],1,6)
@@ -162,9 +171,8 @@ def main():
     miner.setMatrix(result[3],4,9)
     miner.setMatrix(result[4],5,10) 
     writecsv(miner.matrix) # write the result of the genes vs types matrix
-    readcsv() # if updating mongodb, genes file use this
-    print "Matrix Writtn....done"
-
+    #readcsv() # if updating mongodb, genes file use this
+    print "Done"
 
 if __name__ == "__main__":
 	main()
